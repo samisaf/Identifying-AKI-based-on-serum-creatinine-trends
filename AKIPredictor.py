87f680ab8837006060fcc01107a55da2a761bcd0
Sami Safadi <samisaf@gmail.com>
2014-05-04 17:44:50 -0500

    Writes output
    Writes output as csv file. Organizes data into Input and Output folders.

--- AKIPredictor.py
+++ AKIPredictor.py
@@ -8,33 +8,33 @@ ptDict = {} # Dictionary containing all lab values
 Patients = {} # Dictonary, keys are MRN, values are Patient objects
 
 class Patient(object):
-    def __init__(self, mrn: int, esrd: bool = False,  crs: dict = dict()):
+    def __init__(self, mrn: int, data: dict = dict()):
         self.mrn = mrn
-        self.esrd = esrd
-        self.crs = DataFrame(Series(crs), columns=['value'])
-        self.crs.index = pd.to_datetime(self.crs.index)
+        self.data = DataFrame(Series(data), columns=['value'])
+        self.data.index = pd.to_datetime(self.data.index)
+        self.crs = self.data.value
         self.baseCr = self.__calcBaseCr__()
-        self.crs['order'] = range(len(crs))
-        self.crs['slope'] = self.__calcSlopes__()
-        self.crs['peak'] = self.__calcPeaks__()
-        self.crs['aki'] = self.__calcAKI__()
-        self.aki = self.crs.value[self.crs.aki]
+        self.data['order'] = range(len(data))
+        self.data['slope'] = self.__calcSlopes__()
+        self.data['peak'] = self.__calcPeaks__()
+        self.data['aki'] = self.__calcAKI__()
+        self.aki = self.data.value[self.data.aki]
  
     def __str__(self): 
         temp = "<MRN {}, baseline cr {}, crs {}, AKI {}>"\
-            .format(self.mrn, self.baseCr, len(self.crs), sum(self.crs.aki))
+            .format(self.mrn, self.baseCr, len(self.crs), sum(self.data.aki))
         return temp
 
     def __repr__(self): return self.__str__()
     
     def __calcBaseCr__(self):
-        return np.percentile(self.crs.value, 50)
+        return np.percentile(self.crs, 25)
     
     def __calcSlopes__(self):
         slopes = []
         for i in range(len(self.crs)-1):
-            x1, x2 = self.crs.order[i], self.crs.order[i+1]
-            y1, y2 = self.crs.value[i], self.crs.value[i+1]
+            x1, x2 = self.data.order[i], self.data.order[i+1]
+            y1, y2 = self.data.value[i], self.data.value[i+1]
             slopes.append((y2-y1)/(x2-x1))
         # assign slope after last point to the preceding slope
         if len(slopes) == 0: slopes.append(0) 
@@ -43,24 +43,31 @@ class Patient(object):
         
     def __calcPeaks__(self):
         # first point is a peak if following slope is negative
-        peaks = [self.crs.slope[0] <= 0] 
+        peaks = [self.data.slope[0] <= 0] 
         for i in range(1, len(self.crs)):
-            if self.crs.slope[i-1] > 0 and self.crs.slope[i] <= 0: peaks.append(True)
+            if self.data.slope[i-1] > 0 and self.data.slope[i] <= 0: 
+                peaks.append(True)
             else: peaks.append(False)
         return peaks
     
     def __calcAKI__(self):
-        return (self.crs.value > self.baseCr * 1.5) & self.crs.peak
+        return (self.data.value > self.baseCr * 1.5) & self.data.peak
                                
     def plot(self):
-        plt.hlines(self.baseCr, self.crs.index.min(), self.crs.index.max(), linestyles='dotted')
-        plt.plot(self.crs.index, self.crs.value)        
-        plt.plot(self.crs.index, self.crs.value, 'g.')
-        plt.plot(self.crs.index[self.crs.aki], self.crs.value[self.crs.aki], 'ro')
+        plt.hlines(self.baseCr, self.data.index.min(), self.data.index.max(), linestyles='dotted')
+        plt.plot(self.data.index, self.data.value)        
+        plt.plot(self.data.index, self.data.value, 'g.')
+        plt.plot(self.data.index[self.data.aki], self.data.value[self.data.aki], 'ro')
         plt.title(str(self))
         plt.xlabel("Date")
         plt.ylabel("Creatinine")
         plt.show()
+        
+def getNumPatients():
+    return len(Patients)
+    
+def getNumCrs():
+    return sum([Patients[p].crs.size for p in Patients])
      
 def createPtDict(df: DataFrame):
     global ptDict 
@@ -77,9 +84,8 @@ def createPts(patients: dict):
     global Patients
     for key in patients:
         crs = patients[key]
-        try: mrn = int(key)
-        except: mrn = 0
-        if len(crs) > 0: Patients[mrn] = Patient(mrn=mrn, crs=crs)
+        mrn = int(key)
+        if len(crs) > 0: Patients[mrn] = Patient(mrn=mrn, data=crs)
 
 def plotPatients():
     global Patients
@@ -95,21 +101,25 @@ def getNumAKI():
     for key in Patients:
         mrn.append(Patients[key].mrn)
         aki.append(Patients[key].aki.size)
-    di = {'MRN': mrn, 'NumAKI': aki}
+    di = {'MRN': mrn, 'NumOfAKI': aki}
     df = DataFrame(di)
     df['AKI'] = df.NumAKI > 0
     return DataFrame(df)
 
-def start(files:[str]):
+def read(files:[str]):
     global Patients
-    for file in files:
-        df = pd.read_csv(file)
-        createPtDict(df)
-        createPts(ptDict)
+    for file in files: createPtDict(pd.read_csv(file))
+    createPts(ptDict)
+
+def write():
+    outputfile = "Output/aki.csv"
+    getNumAKI().to_csv(outputfile, index=False)
 
 if __name__ == "__main__":
-    files = glob.glob("*.csv")
-    print("The following files are proccessed: ")
-    print(files)
-    start(files)
+    files = glob.glob("Input/*.csv")
+    print("The following files are proccessed: ", files)
+    read(files)
+    print("Processed", getNumPatients(), "patients, and", getNumCrs(), "laboratory values.")
+    print("Writing results to disk")    
+    write()
     input("Done, press enter to exit...")
